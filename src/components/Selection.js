fd75b256caa176cbcb8f15db1736f235b594986f
Tathagat155 <tathagatkumar70618@gmail.com>
2026-04-06 19:02:27 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,31 @@
+import React from "react";
 
+const Selection = ({ color }) => {
+
+  const changeColor = (e) => {
+    e.target.style.backgroundColor = color;
+  };
+
+  return (
+    <div>
+
+      <div
+        className="fix-box"
+        onClick={changeColor}
+      >Selection</div>
+
+      <div
+        className="fix-box"
+        onClick={changeColor}
+      >Selection</div>
+
+      <div
+        className="fix-box"
+        onClick={changeColor}
+      >Selection</div>
+
+    </div>
+  );
+};
+
+export default Selection;
