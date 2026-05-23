20a3b1f475cca2d7b1508d605784ddcdad5f3310
Tathagat155 <tathagatkumar70618@gmail.com>
2026-05-23 14:30:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ const App = () => {
 
       <div className="row">
        
-          <ColourSelectorchangeColor={changeColor}  />
+          <ColourSelector changeColor={changeColor}  />
        
       </div>
 
